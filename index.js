43bd2d85d9d6fa0b38c93c69b52a3ef9a3f2607f
Dennis <dennismusembi344@gmail.com>
2024-09-16 13:39:50 +0300

    redone

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 
-// Write your code here
+// // Write your code here
+// let num1=31
+// let num2=2
+//     let multiply=num1*num2
+
+// const random=Math.floor(Math.random() * 10) + 1;
+// console.log(random)
+
+
+
+// let num3 = 19;
+//     let num4 = 5;
+//     let mod = num3 % num4;
+
+
+
+
+
+// const numbers = [5, 10, 15, 20, 17];
+//     const max = Math.max(...numbers);
+
 let num1=31
 let num2=2
-    let multiply=num1*num2
+let multiply=num1*num2
 
-const random=Math.floor(Math.random() * 10) + 1;
-console.log(random)
-
-
-
-let num3 = 19;
-    let num4 = 5;
-    let mod = num3 % num4;
-
-
-
-
-
-const numbers = [5, 10, 15, 20, 17];
-    const max = Math.max(...numbers);
-    
+let random=Math.floor(Math.random()*10)+1
+let num3=6
+let num4=16
+let mod=num4%num3
+const numbers=[5,10,15,20,17]
+let max=Math.max(...numbers)
